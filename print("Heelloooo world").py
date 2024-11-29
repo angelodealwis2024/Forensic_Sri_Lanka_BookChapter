535e120947747afac224cdff491bb5dd7d575b43
angelodealwis2024 <docbackup.angelo@gmail.com>
2024-11-29 20:57:52 +1000

    Update print("Heelloooo world").py

--- print("Heelloooo world").py
+++ print("Heelloooo world").py
@@ -2,3 +2,8 @@ print("Heelloooo world")
 print("12345")
 def ggr = 6
 print(ggr)
+print("Heelloooo world1")
+print("Heelloooo world2")
+print("Heelloooo world3")
+print("Heelloooo world4")
+print("Heelloooo world5")
